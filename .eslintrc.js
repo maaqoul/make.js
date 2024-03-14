bb2b7e2939d694b5179d38be81ccd4faf49d814e
hicham maaqoul <hicham.maaqoul@gmail.com>
2024-03-14 01:57:18 +0000

    Initial project setup with webpack, babel, eslint, and prettier configuration

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  rules: {
+    // Custom rules here
+  },
+};
