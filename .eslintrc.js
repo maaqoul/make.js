c7357fc74ff80bf24c6eaa5dd79af91cdfeb0ecd
hicham maaqoul <hicham.maaqoul@gmail.com>
2024-03-15 01:10:24 +0000

    add webpack server for HMR

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
   extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+    // Ensure you have a babel config file with the necessary plugins/presets
+    requireConfigFile: false,
+  },
   env: {
     browser: true,
     es2021: true,
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
-const path = require("path");
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import webpack from "webpack";
 
-module.exports = {
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+    clean: true,
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    hot: true,
   },
+  mode: "development",
 };
